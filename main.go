4a21ad44639fbe66434d9cb71b364df0db80d499
Nelo Puchades <correo@nelo.dev>
2022-01-12 22:06:29 +0100

    playing with for loops

--- main.go
+++ main.go
@@ -82,4 +82,30 @@ func main() {
 	planetSliceVerbose = append(planetSliceVerbose, "mercury")
 	planetSliceVerbose = append(planetSliceVerbose, "mars")
 	fmt.Println(planetSliceVerbose)
+
+	// For loop
+	ages := []int{42, 28, 30, 27, 18}
+	for i := 0; i < len(ages); i++ {
+		fmt.Println(ages[i])
+	}
+
+	for i := 0; i < len(ages); {
+		fmt.Println(ages[i])
+		i++
+	}
+
+	// There's no do-while loops in GO
+
+	for i := 0; i < 10; i++ {
+		if i%2 != 0 {
+			continue
+		}
+
+		fmt.Println(i)
+	}
+
+	for index, value := range ages {
+		fmt.Println(index)
+		fmt.Println(value)
+	}
 }
